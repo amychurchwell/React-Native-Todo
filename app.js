2f563ce75d9eae5fe34520fde598a34fe47e71be
Amy Churchwell <amy.churchwell@gmail.com>
2018-03-01 15:08:44 -0800

    Add remove item button

--- app.js
+++ app.js
@@ -18,6 +18,7 @@ class App extends Component {
     this.handleAddItem = this.handleAddItem.bind(this);
     this.handleToggleComplete = this.handleToggleComplete.bind(this);
     this.handleToggleAllComplete = this.handleToggleAllComplete.bind(this);
+    this.handleRemoveItem = this.handleRemoveItem.bind(this);
   }
   setSource(items, itemsDatasource, otherState = {}) {
     this.setState({
@@ -25,6 +26,12 @@ class App extends Component {
       dataSource: this.state.dataSource.cloneWithRows(itemsDatasource),
       ...otherState
     })
+  }
+  handleRemoveItem(key) {
+    const newItems = this.state.items.filter((item) => {
+      return item.key !== key
+    })
+    this.setSource(newItems, newItems);
   }
   handleToggleComplete(key, complete) {
     const newItems = this.state.items.map((item) => {
@@ -75,6 +82,7 @@ class App extends Component {
               return (
                 <Row
                   key={key}
+                  onRemove={() => this.handleRemoveItem(key)}
                   onComplete={(complete) => this.handleToggleComplete(key, complete)}
                   {...value}
                 />
