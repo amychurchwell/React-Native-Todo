f36576456a968287d34f15ee1a309ed096f9c0d9
Amy Churchwell <amy.churchwell@gmail.com>
2018-03-02 09:26:52 -0800

    Add activity indicator (loader) that disappears after all items are loaded

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { View, Text, StyleSheet, Platform, ListView, Keyboard, AsyncStorage } from "react-native";
+import { View, Text, StyleSheet, ActivityIndicator, Platform, ListView, Keyboard, AsyncStorage } from "react-native";
 import Header from "./header";
 import Footer from "./footer";
 import Row from "./row";
@@ -16,6 +16,7 @@ class App extends Component {
     super(props);
     const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
     this.state = {
+      loading: true,
       allComplete: false,
       filter: "ALL",
       value: "",
@@ -35,9 +36,9 @@ class App extends Component {
     AsyncStorage.getItem("items").then((json) => {
       try {
         const items = JSON.parse(json);
-        this.setSource(items, items);
+        this.setSource(items, items, {loading: false});
       } catch(e) {
-
+        this.setState({loading: false})
       }
     })
   }
@@ -129,6 +130,12 @@ class App extends Component {
           filter={this.state.filter}
           onClearComplete={this.handleClearComplete}
         />
+        {this.state.loading &&<View style={styles.loading}>
+          <ActivityIndicator
+            animating
+            size="large"
+          />
+        </View>}
       </View>
     );
   }
@@ -151,6 +158,16 @@ const styles = StyleSheet.create({
   separator: {
     borderWidth: 1,
     borderColor: "#F5F5F5"
+  },
+  loading: {
+    position: "absolute",
+    left: 0,
+    top: 0,
+    right: 0,
+    bottom: 0,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "rgba(0,0,0,.2)"
   }
 })
 export default App;
